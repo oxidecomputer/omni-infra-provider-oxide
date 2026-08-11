3273a6b3610233496cbf6cc1fddc8f346b30a075
Matthew Sanabria <matthew.sanabria@oxide.computer>
2026-08-10 20:50:10 -0400

    fix: use request context for image download
    Removed the `Timeout` on the `http.Client` so that the 15 minute context
    in `ensureImage` is used when downloading the Talos Linux image.
    
    Closes SSE-386.

--- internal/provider/provisioner.go
+++ internal/provider/provisioner.go
@@ -47,9 +47,7 @@ type Provisioner struct {
 func NewProvisioner(oxideClient *oxide.Client) *Provisioner {
 	return &Provisioner{
 		oxideClient: oxideClient,
-		httpClient: &http.Client{
-			Timeout: 30 * time.Second,
-		},
+		httpClient:  &http.Client{},
 	}
 }
 
